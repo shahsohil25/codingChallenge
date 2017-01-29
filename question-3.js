faf6b34ca5f80e52e9b5cedf410c4ecc4352ff3d
Sohil Shah <shahsohil25@gmail.com>
2017-01-30 01:46:25 +0530

    completed first question solution

--- question-3.js
+++ question-3.js
@@ -1,2 +1,17 @@
 // 1.Using Array iteration methods,check if all inputs and selects on a form have a value
 // Hints:Array.slice,Form.querySelector,map,every
+
+
+// check if element has "Default" or "Blank" value
+function checkIfElementHasValue(element) {
+	return element.value !== "" && element.value !== "Default"
+}
+
+
+//function to validate form and logs that form's elements has value or not
+function validateForm() {
+	var form = document.forms[0];
+  var selectElement = form.querySelectorAll('.input');
+	var elementArray = Array.prototype.slice.call(selectElement);
+	console.log("Check if elements has value::", elementArray.every(checkIfElementHasValue));
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Current Folder Stores all workable solutions.
+
+To run angular application -
+
+You can locally do 'npm install -g http-server' and and than do 'http-server -o'.
